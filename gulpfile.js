284e5a5535b59b82875672351aa95cc6edc3c327
Ivan Georgiev <ivan_georgiew@yahoo.com>
2016-07-01 11:22:42 +0300

    Added pump to handle errors

--- gulpfile.js
+++ gulpfile.js
@@ -4,43 +4,79 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
-    cssnano = require('gulp-cssnano')
+    cssnano = require('gulp-cssnano'),
+    pump = require('pump');
 
 gulp.task('norm', function() {
   return gulp.src('node_modules/node-normalize-scss/_normalize.scss')
     .pipe(gulp.dest('src/assets/css'));
 });
 
-gulp.task('css', function () {
-  return gulp.src('src/assets/css/main.scss')
+gulp.task('css', function (cb) {
+  /*return gulp.src('src/assets/css/main.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer('last 4 version'))
     .pipe(gulp.dest('app/assets/css'))
     .pipe(cssnano())
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('app/assets/css'))
-    .pipe(browserSync.reload({stream:true}));
+    .pipe(browserSync.reload({stream:true}));*/
+  pump([
+      gulp.src('src/assets/css/main.scss'),
+      sass(),
+      autoprefixer('last 4 version'),
+      gulp.dest('app/assets/css'),
+      cssnano(),
+      rename({ suffix: '.min' }),
+      gulp.dest('app/assets/css'),
+      browserSync.reload({stream:true})
+    ],
+    cb
+  );
 });
 
-gulp.task('js', function(){
-  return gulp.src('src/assets/js/app.js')
+gulp.task('js', function(cb){
+  /*return gulp.src('src/assets/js/app.js')
     .pipe(gulp.dest('app/assets/js'))
-    .pipe(uglify())
+    .pipe(uglify().on('error', uglify.message))
     .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('app/assets/js'))
-    .pipe(browserSync.reload({stream:true, once:true}));
+    .pipe(browserSync.reload({stream:true, once:true}));*/
+  pump([
+      gulp.src('src/assets/js/app.js'),
+      uglify(),
+      rename({ suffix: '.min' }),
+      gulp.dest('app/assets/js'),
+      browserSync.reload({stream:true, once:true})
+    ],
+    cb
+  );
 });
 
-gulp.task('html', function(){
-  return gulp.src('src/index.html')
+gulp.task('html', function(cb){
+  /*return gulp.src('src/index.html')
     .pipe(gulp.dest('app/'))
-    .pipe(browserSync.reload({stream:true}));
+    .pipe(browserSync.reload({stream:true}));*/
+  pump([
+      gulp.src('src/index.html'),
+      gulp.dest('app/'),
+      browserSync.reload({stream:true})
+    ],
+    cb
+  );
 });
 
-gulp.task('img', function(){
-  return gulp.src('src/assets/img/**/*')
-    .pipe(gulp.dest('app/assets/img/'))
-    .pipe(browserSync.reload({stream:true}));
+gulp.task('img', function(cb){
+  //return gulp.src('src/assets/img/**/*')
+  //  .pipe(gulp.dest('app/assets/img/'))
+  //  .pipe(browserSync.reload({stream:true}));
+  pump([
+      gulp.src('src/assets/img/**/*'),
+      gulp.dest('app/assets/img/'),
+      browserSync.reload({stream:true})
+    ],
+    cb
+  );
 });
 
 gulp.task('browser-sync', function() {
