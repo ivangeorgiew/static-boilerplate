fa39d2bd79f1f9b4e5274aa4271716c8b99b6888
Ivan Georgiev <ivan_georgiew@yahoo.com>
2016-12-09 10:15:33 +0200

    Removed babel

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,7 @@ var gulp = require('gulp'),
     cssnano = require('gulp-cssnano'),
     concat = require('gulp-concat'),
     pump = require('pump');
-    babel = require('gulp-babel');
 
-  //gulp.task('norm', function() {
-  //  return gulp.src('node_modules/node-normalize-scss/_normalize.scss')
-  //    .pipe(gulp.dest('src/assets/css'));
-  //});
 
 gulp.task('css', function (cb) {
   pump([
@@ -28,28 +23,10 @@ gulp.task('css', function (cb) {
   );
 });
 
-//gulp.task('jQuery', function(){
-//  return gulp.src('node_modules/jquery/dist/jquery.min.js')
-//    .pipe(gulp.dest('app/assets/js'));
-//});
-
-//gulp.task('js', function(cb){
-//  pump([
-//     gulp.src('src/assets/js/app.js'),
-//     uglify({mangle:false}),
-//     rename('src/assets/js/ugly.js'),
-//     gulp.dest('src/assets/js'),
-//     browserSync.reload({stream:true, once:true})
-//   ],
-//   cb
-//  );
-//});
-
 gulp.task('jsConcat', function(cb){
   pump([
     gulp.src(['node_modules/jquery/dist/jquery.min.js', 'src/assets/js/app.js']),
     concat('app.min.js'),
-    babel(),
     gulp.dest('app/assets/js'),
     browserSync.reload({stream:true})
     ],
@@ -90,10 +67,7 @@ gulp.task('bs-reload', function () {
 });
 
 gulp.task('default', ['css', 'jsConcat', 'html', 'img', 'browser-sync'], function () {
-    //gulp.watch('node_modules/node-normalize-scss/_normalize.scss', ['norm']);
     gulp.watch("src/assets/css/*.scss", ['css']);
-    //gulp.watch("node_modules/jquery/dist/jquery.min.js", ['jQuery']);
-    //gulp.watch("src/assets/js/app.js", ['js']);
     gulp.watch("src/assets/js/app.js", ['jsConcat']);
     gulp.watch("src/*.html", ['html']);
     gulp.watch("src/assets/img/**/*", ['img']);
